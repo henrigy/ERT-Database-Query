7e4f20f9341fe7bd501184e0f5acd1a6d50fda5b
Henry <henryliu123@outlook.com>
2021-08-05 14:55:17 -0700

    create add data layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,55 @@
 import "./App.css";
+import Title from "./Banner.js";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div class="App">
+			<Title />
+			{/*area for search bar, add entry button, & display all entries button*/}
+			<div class="searchArea">
+				<div class="topRow">
+					<h2>PVGS Electronic Rework Team Query</h2>
+					<div class="searchInput">
+						<div class="input">
+							<input autoComplete="off" style={{ paddingLeft: "3px" }}></input>
+							<button>Search</button>
+						</div>
+					</div>
+				</div>
+				<div class="moreButtons">
+					<button>Create Data Entry</button>
+					<button>Display All Data Entries</button>
+				</div>
+			</div>
+
+			{/*area for adding a data entry*/}
+			<div class="addingEntry">
+				<h2>Create Data Entry</h2>
+				<p class="line1"></p>
+				<div class="box">
+					<h4>FTA Number:</h4>
+					<input />
+					<h4>FTA Revision:</h4>
+					<input />
+					<h4>Test Fixture:</h4>
+					<input />
+					<h4>Reference(s):</h4>
+					<input />
+					<h4>Part Number:</h4>
+					<input />
+					<h4>Part Revision:</h4>
+					<input />
+					<h4>APN Number</h4>
+					<input />
+					<h4>Part Description</h4>
+					<input />
+					<div class="addDataButton">
+						<button>Add Data Entry</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/Banner.js
@@ -0,0 +1,14 @@
+import "./Banner.css";
+
+const Banner = () => {
+	return (
+		<div className="title">
+			<img
+				src="https://www.maricopa.gov/ImageRepository/Document?documentID=32577"
+				alt="PVGS logo"
+			/>
+		</div>
+	);
+};
+
+export default Banner;
